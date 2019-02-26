9b6869967d6a07316f5bfcd2eaacde71a788d2ec
Meredith Hu <33817177+meredeath@users.noreply.github.com>
2019-02-25 19:00:42 -0500

    Update main.py
    the matrix I multiplied by was wrong!

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from matrix import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
-matrixx = [[1.1,1.1,1.1],[1.1,1.1,1.1],[1.1,1.1,1.1],[1.1,1.1,1.1],[1.1,1.1,1.1],[1.1,1.1,1.1],[1.1,1.1,1.1],[1.1,1.1,1.1]]
+matrixx = [[1.1,0,0,0],
+           [0,1.1,0,0],
+           [0,0,1.1,0],
+           [0,0,0,1.1]]
 
 add_edge(matrix,100,100,0,100,200,0)
 add_edge(matrix,100,100,0,200,100,0)
@@ -13,13 +16,13 @@ add_edge(matrix,100,200,0,200,200,0)
 add_edge(matrix,200,100,0,200,200,0)
 draw_lines( matrix, screen, color )
 
-for i in range(1):
-    print("matrix before: \n")
-    print_matrix(matrix)
+for i in range(50):
+    #print("matrix before: \n")
+    #print_matrix(matrix)
     matrix_mult(matrixx,matrix)
 
-    print("matrix after: \n")
-    print_matrix(matrix)
+    #print("matrix after: \n")
+    #print_matrix(matrix)
     draw_lines(matrix,screen,color)
 
 display(screen)
