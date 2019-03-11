356faa8ca2e42b4750237191826f2d5b49241472
Meredith <mhu2@stuy.edu>
2019-03-11 11:00:39 -0400

    fixing the matrix mult function

--- matrix.py
+++ matrix.py
@@ -37,9 +37,9 @@ def ident( matrix ):
     return
 
 #few tests I'm just going to comment out
-#m1 = [[0,1,2,3],[0,1,2,3],[0,1,2,3],[2,7,4,8]]
+m1 = [[0,1,2,3],[0,1,2,3],[0,1,2,3],[2,7,4,8]]
 #ident(m1)
-#m2 = [[3,4,5,6],[2,4,8,6],[35,87,9,5],[34,7,53,8]]
+m2 = [[3,4,5,6],[2,4,8,6],[35,87,9,5],[34,7,53,8]]
 
 #multiply m1 by m2, modifying m2 to be the product
 #m1 * m2 -> m2
@@ -48,6 +48,7 @@ def matrix_mult( m1, m2 ):
     if len(m1[0]) != len(m2):
         return
     else:
+        '''
         counter = len(m2)
         for i in range(counter):
             element = []
@@ -57,6 +58,27 @@ def matrix_mult( m1, m2 ):
                     n += m1[k][j] * m2[i][k]
                 element.append(n)
             m2[i] = element
+        '''
+        #for each row in the first matrix, we're going to take the dot product
+        m1row = 0
+        m2col = 0
+        while m1row < len(m1):
+            while m2col < len(m1):
+                s = 0 #the sum so far
+                counter = 0
+                while counter < len(m1[0]):
+                    s += m1[m1row][counter] * m2[counter][m1row]
+                    counter += 1
+                m2[m1row][m2col] = s
+                m2col += 1
+            m1row += 1
+
+print("before: \n")
+print_matrix(m1)
+print_matrix(m2)
+matrix_mult(m1,m2)
+print("\nafter\n")
+print_matrix(m2)
         
 def new_matrix(rows = 4, cols = 4):
     m = []
